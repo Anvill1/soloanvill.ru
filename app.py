30168b7ef7841cd2b7614bc6bf9ba90bf5fe64e3
timur.ramonov <timur.ramonov@monopoly.su>
2024-01-19 23:30:27 +0300

    feat: добавлен prometheus exporter

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, redirect
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 app.secret_key = 'Ol91xL&fP],dfsg1)L1X#K%dsgdfgdf'
 
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/metrics')
+def metrics():
+    return metrics.export()
 
 
 if __name__ == '__main__':
