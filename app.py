2d4fbd35dab084bb3cae41797385f9d80e881384
timur.ramonov <timur.ramonov@monopoly.su>
2024-06-14 19:03:20 +0300

    feat: Добавлен POST запрос редеплоя проекта

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
+import requests, logging
 from flask import Flask, render_template, request, redirect
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 app.secret_key = 'Ol91xL&fP],dfsg1)L1X#K%dsgdfgdf'
 
+logging.basicConfig(level=logging.INFO)
+
+backend_url = 'http://127.0.0.1:8080/api/deploys/create'
+
 metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Application info', version='1.0.3')
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        username = request.form['username'].rstrip()
+        email = request.form['email']
+        ip = request.remote_addr
+        data = {
+            'username': username,
+            'email': email,
+            'ip': ip
+        }
+        try:
+            response = requests.post(backend_url, json=data)
+            response.raise_for_status()
+            app.logger.info(f"Redeploy request by {username} created successfully")
+        except requests.exceptions.HTTPError as http_err:
+            app.logger.error(f"HTTP ошибка: {http_err}")
+        except Exception as err:
+            app.logger.error(f"Другая ошибка: {err}")
+        return redirect('/')
+    else:
+        return render_template("index.html")
 
 @app.route('/metrics')
 def metrics():
